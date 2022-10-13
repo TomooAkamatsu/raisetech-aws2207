05f78e4dc4ff872977a37f91c1682bc76d7d46ac
TomooAkamatsu <t.akmt@ymail.ne.jp>
2022-10-13 11:49:21 +0900

    vpcとsubnetを一つのモジュールにまとめて修正

--- lecture12/terraform/dev/main.tf
+++ lecture12/terraform/dev/main.tf
@@ -1,43 +1,37 @@
-module "vpc" {
-  source = "../modules/vpc"
-}
+module "vpc_subnet" {
+  source = "../modules/vpc-subnet"
 
-module "subnet" {
-  source = "../modules/subnet"
-
-  vpc_id = module.vpc.vpc_id
+  cidr_block = "10.0.0.0/16"
 }
 
 module "routing" {
   source = "../modules/routing"
 
-  vpc_id          = module.vpc.vpc_id
-  public_subnet_a = module.subnet.public_subnet_a
-  public_subnet_c = module.subnet.public_subnet_c
+  vpc_id          = module.vpc_subnet.vpc_id
+  public_subnet_a = module.vpc_subnet.public_subnet_a
+  public_subnet_c = module.vpc_subnet.public_subnet_c
 }
 
 module "ec2" {
   source = "../modules/ec2"
 
-  vpc_id          = module.vpc.vpc_id
-  public_subnet_a = module.subnet.public_subnet_a
+  vpc_id          = module.vpc_subnet.vpc_id
+  public_subnet_a = module.vpc_subnet.public_subnet_a
 }
 
 module "rds" {
   source = "../modules/rds"
 
-  vpc_id               = module.vpc.vpc_id
-  db_subnet_group_name = module.subnet.db_subnet_group
+  vpc_id               = module.vpc_subnet.vpc_id
+  db_subnet_group_name = module.vpc_subnet.db_subnet_group
   db_password          = "password"
 }
 
 module "alb" {
   source = "../modules/alb"
 
-  vpc_id          = module.vpc.vpc_id
-  public_subnet_a = module.subnet.public_subnet_a
-  public_subnet_c = module.subnet.public_subnet_c
+  vpc_id          = module.vpc_subnet.vpc_id
+  public_subnet_a = module.vpc_subnet.public_subnet_a
+  public_subnet_c = module.vpc_subnet.public_subnet_c
   ec2_id          = module.ec2.ec2_id
 }
-
-
